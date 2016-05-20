70c5be73d55581b3af4d2ed409951ee9acaab241
Yu Guo <yuguo@uber.com>
2016-05-19 22:19:27 -0700

    Initial Commit

---
+++ PitchPerfect/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PitchPerfect
+//
+//  Created by Yu Guo on 5/19/16.
+//  Copyright © 2016 Yu Guo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
